30184f8ae4ba776ef693d4c715e7c284f9283da0
Yranildo Oliveira <116233090+YranildoSilva@users.noreply.github.com>
2022-11-14 17:50:46 -0300

    SQL
    Exercício SQL

---
+++ Aula-01-SELEC.sql
@@ -0,0 +1,15 @@
+/* SELECT *
+
+FROM person.Person; */
+
+/*select title
+from person.Person;*/
+
+/* 
+EXERCICIO 1
+A equipe de marketing precisa de fazer uma pesquisa sobre nomes mais comuns de seus
+clientes e precisa do nome e sobrenome de todos os clientes que estão cadastrados no sitema.*/
+
+SELECT firstname, LastName
+
+FROM person.Person;
